a42e3e16a9e49a2018a2bcda2f92bfd764812aaf
Jason J <iam@ovrhere.com>
2014-09-21 17:54:03 -0400

    Added request + parser Wrapper & added async model for this app
    [Add]: Created Wrapper to easily contain any changes necessary for
    request + parsing interaction. More importantly, it provides a means to
    "pause" the output until the model can reattach to the thread it is
    running in.
    [Add]: Added the primary AsyncModel for this application. It receives
    requests and publishes results, errors, etc. in a simple form.

---
+++ src/com/ovrhere/android/careerstack/model/CareerStackRequestParserWrapper.java
@@ -0,0 +1,238 @@
+package com.ovrhere.android.careerstack.model;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.SocketTimeoutException;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import org.xmlpull.v1.XmlPullParserException;
+
+import android.util.Log;
+
+import com.ovrhere.android.careerstack.dao.CareerItem;
+import com.ovrhere.android.careerstack.model.careersstackoverflow.CareerStackOverflowRssRequest;
+import com.ovrhere.android.careerstack.model.careersstackoverflow.CareersStackOverflowRssXmlParser;
+import com.ovrhere.android.careerstack.model.requests.AbstractSimpleHttpRequest;
+
+/** The request + parser wrapper for easier handling (as both
+ * will happen on the same thread. This allows for pauses and resuming of the 
+ * process.
+ * @author Jason J.
+ * @version 0.1.1-20140921	 */
+class CareerStackRequestParserWrapper implements Runnable,
+	AbstractSimpleHttpRequest.OnRequestEventListener {
+	/** Class name for debugging purposes. */
+	final static private String LOGTAG = 
+				CareerStackRequestParserWrapper.class.getSimpleName();
+	/** Debugging bool. */
+	final static private boolean DEBUG = true;
+	
+	/////////////////////////////////////////////////////////////////////////////////////////////////
+	/// End constants
+	////////////////////////////////////////////////////////////////////////////////////////////////
+	
+	/** The the task has started. */
+	final private AtomicBoolean running = new AtomicBoolean(false);	
+	/** The actual request. */
+	final private AbstractSimpleHttpRequest mRequest;	
+	/** The timeout period. */
+	final private long TIMEOUT;
+	
+	/** The current parser. */
+	private CareersStackOverflowRssXmlParser mParser =  null;
+	
+	/** The final results. Default is null. */
+	private List<CareerItem> resultItems = null;
+	/** The current start time. */
+	private long startTime = -1;	
+
+	
+	/** The results listener. */
+	volatile private OnFeedbackListener mListener = null;
+	/** If the runnable is currently paused. */
+	volatile private boolean isPaused = false;
+		
+	
+	/** Wraps the {@link CareerStackOverflowRssRequest} & 
+	 * {@link CareersStackOverflowRssXmlParser} to handle both in same runnable.
+	 * @param request The request to send
+	 * @param listener The feedback listener; for results and more. 
+	 */
+	public CareerStackRequestParserWrapper(AbstractSimpleHttpRequest request,
+			OnFeedbackListener listener){
+		this.mListener = listener;
+		this.mRequest = request;
+		this.mRequest.setOnRequestEventListener(this);
+		this.TIMEOUT = request.getRequestTimeout();
+	}
+	/** Set results listener. */
+	public void setFeedbackListener(OnFeedbackListener listener) {
+		this.mListener = listener;
+	}
+	/** Note this will cancel the entire thread, causing it to throw an 
+	 * InterruptedException. Will null all InputStreams. */
+	public void cancel(){
+		pauseParser(false); //always must be running at cancel
+		mRequest.cancel();
+	}
+	
+	/** Pauses the wrapper results response. 
+	 * If <code>pause > request timeout</code>, it gives up. Same as calling
+	 * {@link #pause(boolean)} with <code>false</code>. */
+	public void pause(){
+		pause(false);
+	}
+	
+	/** Pauses the request/parsing. If <code>pause > request timeout</code>, it gives up.		 
+	 * @param force <code>true</code> to force a full pause (with results)
+	 * <code>false</code> to only pause the wrapper/results*/
+	public void pause(boolean force){
+		isPaused = true;
+		if (force){
+			pauseParser(true);
+		}
+		if (DEBUG){
+			Log.d(LOGTAG, "paused");
+		}	
+	}
+	
+	/** Resumes the request/parsing. */
+	public void resume(){	
+		isPaused = false;
+		pauseParser(false);
+		if (DEBUG){
+			Log.d(LOGTAG, "resumed");
+		}	
+	}
+	
+	/** Returns <code>true</code> if #run() has run/is running. */
+	public boolean isRunning(){
+		return running.get();
+	}
+	
+	/////////////////////////////////////////////////////////////////////////////////////////////////
+	/// Helper methods.
+	////////////////////////////////////////////////////////////////////////////////////////////////
+	/** Pauses and resumes parsed based on value of pause
+	 *  @param pause <code>true</code> to pause, <code>false</code> to resume
+	 */
+	private void pauseParser(boolean pause){
+		if (mParser != null){
+			if (pause){
+				mParser.pause();
+			} else {
+				mParser.resume();
+			}
+		} 
+	}
+	
+	/** Blocking. Checks the pause time every 200 ms. If pause still waiting
+	 * after timeout time, return false.
+	 * @return <code>true</code> if to continue is lifted, 
+	 * <code>false</code> if we are still paused or thread interupted.
+	 * @throws InterruptedException
+	 */
+	private boolean checkContinue() throws InterruptedException{
+		final int waitTime = 200;
+		while (isPaused){
+			if (DEBUG){
+				Log.d(LOGTAG, "Sleeping for " + waitTime + " millis  ");
+			}
+			Thread.sleep(waitTime);
+			if (System.currentTimeMillis() - startTime > TIMEOUT + waitTime ){
+				return false;
+			}
+		}
+		if (Thread.interrupted()){
+			return false;
+		}
+		return true;
+	}
+	
+	/////////////////////////////////////////////////////////////////////////////////////////////////
+	/// Internal listeners
+	////////////////////////////////////////////////////////////////////////////////////////////////
+	/** The results interface; sends feedback on the entire procedure.
+	 * @version 0.1.0-20140919 */
+	public static interface OnFeedbackListener {
+		/** Publishes results when available. */
+		public void onResults(List<CareerItem> careerList);
+		/** Sends exceptions if encountered. */
+		public void onException(Exception exception);
+	}
+	
+	/////////////////////////////////////////////////////////////////////////////////////////////////
+	/// Override methods
+	////////////////////////////////////////////////////////////////////////////////////////////////
+	
+	@Override
+	public void run() {
+		try {
+			running.set(true);;
+			isPaused = false;
+			startTime = System.currentTimeMillis();
+			mRequest.run();
+		} catch (Exception e){
+			running.set(false);
+			mListener.onException(e);
+		}
+	}
+	
+	@Override
+	public void onStart(InputStream in) {
+		try {	
+			if (checkContinue()){
+				mParser = new CareersStackOverflowRssXmlParser();
+				resultItems = mParser.parseXmlStream(in);
+			}
+			checkContinue();
+			
+		} catch (XmlPullParserException e) {
+			mListener.onException(e);
+		} catch (IOException e) {
+			mListener.onException(e);
+		} catch (InterruptedException e) {
+			mListener.onException(e);
+		} finally {
+			running.set(false);
+		}
+	}	
+	
+	@Override
+	public void onResponseCode(int responseCode) {
+		if (DEBUG){
+			Log.d(LOGTAG, "Reponse code: " + responseCode);
+		}		
+	}
+	
+	@Override
+	public void onException(Exception exception) {
+		if (DEBUG){
+			Log.d(LOGTAG, "Exception in object: " +this.hashCode());
+		}
+		mListener.onException(exception);
+		running.set(false);
+	}
+	
+	@Override
+	public void onComplete() {
+		if (DEBUG){
+			Log.d(LOGTAG, "request: onComplete in: " + this.hashCode());
+		}
+		try {
+			if (checkContinue()){
+				if (resultItems != null){
+					mListener.onResults(resultItems);
+				} else {
+					//we will give a timeout
+					mListener.onException(new SocketTimeoutException());
+				}
+			}			
+		} catch (InterruptedException e) {
+			mListener.onException(e);
+		} finally {
+			running.set(false);
+		}
+	}
+}
---
+++ src/com/ovrhere/android/careerstack/model/CareersStackOverflowModel.java
@@ -0,0 +1,315 @@
+/*
+ * Copyright 2014 Jason J.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.ovrhere.android.careerstack.model;
+
+import java.io.IOException;
+import java.net.SocketTimeoutException;
+import java.util.List;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.util.Log;
+
+import com.ovrhere.android.careerstack.dao.CareerItem;
+import com.ovrhere.android.careerstack.model.asyncmodel.AsyncModel;
+import com.ovrhere.android.careerstack.model.asyncmodel.RunnableHeadlessFragment;
+import com.ovrhere.android.careerstack.model.careersstackoverflow.CareerStackOverflowRssRequest;
+
+/** Async model for the stack overflow rss career feed.
+ * Ensure that you call #dispose() in onDestroy().
+ * @author Jason J.
+ * @version 0.2.0-20140921
+ */
+public class CareersStackOverflowModel extends AsyncModel 
+	implements CareerStackRequestParserWrapper.OnFeedbackListener {
+	/** Class name for debugging purposes. */
+	final static private String CLASS_NAME = CareersStackOverflowModel.class
+			.getSimpleName();
+	/** The log tag to use. */
+	final static private String LOGTAG = CLASS_NAME;
+	/** The boolean for debugging. */
+	final static private boolean DEBUG = true;
+	/** The boolean for verbose debugging. */
+	final static private boolean VERBOSE = false;
+	
+	/** The tag for this fragment. */
+	final static private String RUNNABLE_HEADLESS_FRAG_TAG = 
+			CLASS_NAME+"."+RunnableHeadlessFragment.class.getSimpleName();
+	
+	/////////////////////////////////////////////////////////////////////////////////////////////////
+	/// Start public constants
+	////////////////////////////////////////////////////////////////////////////////////////////////
+	
+	/** Bundle key. The value of tags. String */
+	final static public String KEY_TAG_TEXT = 
+			CLASS_NAME + ".KEY_TAG_TEXT";
+	/** Bundle key. The value of keyword. String */
+	final static public String KEY_KEYWORD_TEXT = 
+			CLASS_NAME + ".KEY_KEYWORD_TEXT";
+	/** Bundle key. The value of location. String. */
+	final static public String KEY_LOCATION_TEXT = 
+			CLASS_NAME + ".KEY_LOCATION_TEXT";
+	/** Bundle key. The whether the remote check is set. Boolean. */
+	final static public String KEY_REMOTE_ALLOWED = 
+			CLASS_NAME + ".KEY_REMOTE_ALLOWED";
+	/** Bundle key. The whether the relocation check is set. Boolean. */
+	final static public String KEY_RELOCATE_OFFER = 
+			CLASS_NAME + ".KEY_RELOCATE_OFFER";
+	/** Bundle Key. The query distance. Int. */
+	final static public String KEY_DISTANCE = 
+			CLASS_NAME + ".KEY_DISTANCE";
+	/** Bundle Key. Whether to use km (true) or miles (false). 
+	 * If omitted, miles assumed. Boolean. */
+	final static public String KEY_USE_METRIC = 
+			CLASS_NAME + ".KEY_USE_METRIC";
+	
+	/////////////////////////////////////////////////////////////////////////////////////////////////
+	/// End keys
+	////////////////////////////////////////////////////////////////////////////////////////////////
+	/** Queries an rss feed for results. Accompanied by {@link Bundle} with
+	 * keys. Responds via {@link #REPLY_RECORDS_RESULT}. 
+	 * <p>This query is paused during
+	 * {@link #dispose()} and would need resuming via 
+	 * {@link #REQUEST_RESUME_QUERY}.*/
+	final static public int REQUEST_RECORD_QUERY = 0x001;
+	
+	/** Requests that any running queries be paused. Note that
+	 * the results are paused, the query runs. */
+	final static public int REQUEST_PAUSE_QUERY = 0x002;
+	/** Requests that any paused queries be resumed. */
+	final static public int REQUEST_RESUME_QUERY = 0x003;
+	
+	/** Requests that the query be cancelled. */
+	final static public int REQUEST_QUERY_CANCEL = 0x004;
+	
+	/** Reply of relevant records. 
+	 * Accompanied by a {@link List} of {@link CareerItem}. */
+	final static public int REPLY_RECORDS_RESULT = 0x101;
+	
+	/** Notification for starting update from web.*/
+	final static public int NOTIFY_STARTING_QUERY = 0x201;
+	/** Notification for paused query. */
+	final static public int NOTIFY_QUERY_PAUSED = 0x202;
+	/** Notification for resuming query.*/
+	final static public int NOTIFY_QUERY_RESUMED = 0x203;
+	/** Notification for cancelling query.*/
+	final static public int NOTIFY_CANCELLED_QUERY = 0x204;
+	
+	/** The error for when a request times out. */
+	final static public int ERROR_REQUEST_TIMEOUT = 0x404;
+	
+	/////////////////////////////////////////////////////////////////////////////////////////////////
+	/// End constants
+	////////////////////////////////////////////////////////////////////////////////////////////////
+	/** The fragment manager for getting the fragment. */
+	private FragmentManager mfragManager = null;
+	/** The current request wrapper. */
+	private CareerStackRequestParserWrapper currentRequestWrapper =  null;
+	
+	/** Initializes asynchronous model. 
+	 * @param activity The application's activity. */
+	public CareersStackOverflowModel(FragmentActivity activity) {
+		this.mfragManager = activity.getSupportFragmentManager();
+		getHeadlessFrag();
+		//prepare the headless fragment in advance.
+		RunnableHeadlessFragment runFrag = getHeadlessFrag();
+		if (runFrag.getRunnable() != null){ //if there are any runnables
+			try {
+				currentRequestWrapper = (CareerStackRequestParserWrapper) 
+						runFrag.getRunnable();
+				//update any running to the current object
+				currentRequestWrapper.setFeedbackListener(this);
+			} catch (ClassCastException e){}
+		}
+	}
+	
+	@Override
+	public int sendMessage(int what, Bundle data) {
+		switch (what) {
+		case REQUEST_RECORD_QUERY:
+			buildAndSendRequest(data);
+			return 0;
+		default:
+			//break;
+		}
+		return super.sendMessage(what, data);
+	}
+	
+	@Override
+	public int sendMessage(int what, Object object) {
+		switch (what) {
+		case REQUEST_QUERY_CANCEL:
+			if (currentRequestWrapper != null){
+				currentRequestWrapper.cancel();
+			}
+			notifyHandlers(REQUEST_QUERY_CANCEL, null);
+			return 0;
+		case REQUEST_PAUSE_QUERY:
+			if (currentRequestWrapper != null && currentRequestWrapper.isRunning()){
+				currentRequestWrapper.pause();
+			}
+			notifyHandlers(NOTIFY_QUERY_PAUSED, null);
+			return 0;
+		case REQUEST_RESUME_QUERY:
+			if (currentRequestWrapper != null && currentRequestWrapper.isRunning()){
+				currentRequestWrapper.resume();
+			}
+			notifyHandlers(NOTIFY_QUERY_RESUMED, null);
+			return 0;	
+		default:
+			//break;
+		}
+		return -1;
+	}
+	
+	@Override
+	public void dispose() {
+		super.dispose();
+		if (currentRequestWrapper != null){
+			currentRequestWrapper.pause();
+		}
+		mfragManager = null;
+		if (DEBUG){
+			Log.d(LOGTAG, "disposed");
+		}
+	}
+	
+	/////////////////////////////////////////////////////////////////////////////////////////////////
+	/// Helper methods
+	////////////////////////////////////////////////////////////////////////////////////////////////
+	/** Starts and sends request. 
+	 * @param args The arguments for the request.
+	 */
+	private void buildAndSendRequest(Bundle args){
+		CareerStackOverflowRssRequest request = buildQuery(args);
+		if (DEBUG){
+			Log.d(LOGTAG, "Processing request: " + request.toString());
+		}		
+		currentRequestWrapper = new CareerStackRequestParserWrapper(request, this);
+		
+		RunnableHeadlessFragment frag = getHeadlessFrag();
+		try {
+			CareerStackRequestParserWrapper req = 
+					(CareerStackRequestParserWrapper) frag.getRunnable();
+			if (req != null && req.isRunning()){
+				req.cancel(); 
+				//cancel any pre-running requests
+			}
+		} catch (ClassCastException e){};
+		frag.setRunable(currentRequestWrapper);
+		frag.startThread();
+		//notify listeners
+		notifyHandlers(NOTIFY_STARTING_QUERY, null);
+	}
+	
+	/** Using the {@link CareerStackOverflowRssRequest#Builder} a request is build
+	 * based upon the arguments supplied in the bundle.
+	 * @param args The args of the query.
+	 * @return The final runnable query waiting to be run.	 */
+	private CareerStackOverflowRssRequest buildQuery(Bundle args){
+		CareerStackOverflowRssRequest.Builder queryBuilder = 
+				new CareerStackOverflowRssRequest.Builder();
+		//string
+		String tags = args.getString(KEY_TAG_TEXT);
+		String keywords = args.getString(KEY_KEYWORD_TEXT);
+		String location = args.getString(KEY_LOCATION_TEXT);
+		//checks
+		boolean remoteAllowed = args.getBoolean(KEY_REMOTE_ALLOWED);
+		boolean relocationOffered = args.getBoolean(KEY_RELOCATE_OFFER);
+		//distance
+		int distance = args.getInt(KEY_DISTANCE, -1);
+		boolean useMetric =  args.getBoolean(KEY_USE_METRIC);
+		boolean locationPresent = false;
+		
+		if (tags != null && !tags.isEmpty()){
+			queryBuilder.setTags(tags);
+		}
+		if (keywords != null && !keywords.isEmpty()){
+			queryBuilder.setSearchTerm(keywords);
+		}
+		if (location != null && !location.isEmpty()){
+			queryBuilder.setLocation(location);
+			locationPresent = true;
+		}
+		//semantically the same as if we checked it.
+		queryBuilder.setRemote(remoteAllowed);
+		queryBuilder.setRelocation(relocationOffered);
+		
+		//no distance without location
+		if (locationPresent && distance > 0){
+			queryBuilder.setDistance(distance, useMetric);
+		}
+		
+		return queryBuilder.create();
+	}
+	
+	/** Gets headless fragment if found in manager. Otherwise, places it into 
+	 * the manager
+	 * @return The headless fragment to attach runnables to.	 */
+	private RunnableHeadlessFragment getHeadlessFrag() {
+		RunnableHeadlessFragment runFrag = (RunnableHeadlessFragment)
+				mfragManager.findFragmentByTag(RUNNABLE_HEADLESS_FRAG_TAG);
+		if (runFrag == null ){
+			runFrag = new RunnableHeadlessFragment();
+			mfragManager.beginTransaction()
+					.add(runFrag, RUNNABLE_HEADLESS_FRAG_TAG)
+					.commit();
+		}		
+		return runFrag;
+	}
+	
+	/////////////////////////////////////////////////////////////////////////////////////////////////
+	/// Implemented listeners
+	////////////////////////////////////////////////////////////////////////////////////////////////
+	
+	@Override
+	public void onResults(List<CareerItem> careerList) {
+		notifyHandlers(REPLY_RECORDS_RESULT, careerList);
+		currentRequestWrapper = null;
+		getHeadlessFrag().setRunable(null);	
+	}
+	
+	@SuppressWarnings("unused")
+	@Override
+	public void onException(Exception exception) {
+		if (DEBUG){
+			Log.d(LOGTAG, "Exception in object: " +this.hashCode());
+		}
+		try {
+			throw exception;
+		} catch  (SocketTimeoutException e){
+			if (DEBUG && VERBOSE){ 
+				Log.e(LOGTAG, "Timeout: "+e);
+				e.printStackTrace();
+			}
+			notifyHandlers(ERROR_REQUEST_TIMEOUT, null);
+		} catch (IOException e){
+			if (DEBUG && VERBOSE){
+				Log.e(LOGTAG, "IO exception, likely the xml giving up: "+e);
+				e.printStackTrace();
+			}
+			notifyHandlers(ERROR_REQUEST_FAILED, null);
+		} catch (Exception e){
+			if (DEBUG && VERBOSE){
+				Log.e(LOGTAG, "This. This should not be happening... "+e);
+				e.printStackTrace();
+			}
+			notifyHandlers(ERROR_REQUEST_FAILED, null);
+		}
+	}
+
+}
